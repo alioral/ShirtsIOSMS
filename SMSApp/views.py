4c3e63864efcdb24766cd0b0ed7061ec3f802d33
Ali Oral <alioral@Ali-MacBook-Pro.local>
2013-07-23 20:16:07 +0300

    fix

--- SMSApp/views.py
+++ SMSApp/views.py
@@ -5,9 +5,16 @@ from django_twilio.decorators import twilio_view
 @twilio_view
 def reply_to_sms_messages(request):
 	
+    print 'Printing parameters'
+    
     for i in request:
         print i
-        
+
+    try:
+        messageToSend = request['Body']
+    except:
+        messageToSend = 'An error occured'
     r = Response()
-    r.sms('Thanks for the SMS message!')
+    r.sms(messageToSend)
+
     return r
