c01e6ac0b99a5c69812fb30b842d477d5edc1e45
Ali Oral <alioral@Ali-MacBook-Pro.local>
2013-07-23 20:01:52 +0300

    fix

--- SMSApp/views.py
+++ SMSApp/views.py
@@ -4,8 +4,10 @@ from django_twilio.decorators import twilio_view
 
 @twilio_view
 def reply_to_sms_messages(request):
+	
 	for i in request:
 		print i + ' ' + request[i]
+    
     r = Response()
     r.sms('Thanks for the SMS message!')
     return r
