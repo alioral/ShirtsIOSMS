1694a03bad4d053e428d2f813b2c35fe111bfbae
Ali Oral <alioral@Ali-MacBook-Pro.local>
2013-07-24 03:51:43 +0300

    fix

--- SMSApp/views.py
+++ SMSApp/views.py
@@ -36,7 +36,19 @@ def reply_to_sms_messages(request):
                     requestMapping = helper.returnOrderMappings()
                     print requestMapping
                     getResponse = helper.makeRequest('POST', 'https://www.shirts.io/api/v1/order/', 
-                        requestMapping)
+                        {'api_key': '63aae231cdab277428c0c4e73ee2e9f3ccbe6c42',
+                        'test':'True', 'design':'True', 'price':'18.41', 
+                        'garment[0][product_id]': '3',
+                        'garment[0][color]':'White',
+                        'garment[0][sizes][lrg]': '1',
+                        'print[front][color_count]':'1',
+                        'print[front][artwork]':'http://i488.photobucket.com/albums/rr249/STACIA_GIRL_2009/ANIMALES/koala.png',
+                        'print[front][proof]':'http://www.stanford.edu/~jay/koalas/Koala450j.jpg',
+                        'addresses[0][name]':'John Doe',
+                        'addresses[0][address]':'123 Hope Ln.',
+                        'addresses[0][city]':'Las Vegas',
+                        'addresses[0][state]':'Nevada',
+                        'addresses[0][zipcode]':'12345'})
                     print 'JSON' + getResponse.json()
                     msg = helper.returnText(getResponse.json())
 
